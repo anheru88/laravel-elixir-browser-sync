ac72bcc2dd11c7e09f42d82d5003fe1bcff6aaea
Anheru88 <ajimenez@digitalcaribe.com.co>
2015-05-30 13:53:55 -0500

    Fixed the problem when use gulp watch, the watcher can't found

--- index.js
+++ index.js
@@ -72,7 +72,17 @@ var initializePlugin = function(){
         tasksToRun = _.intersection(config.tasks, _.keys(srcPaths).concat('copy'));
 
         inSequence.apply(this, tasksToRun.concat('watch-assets'));
-    })
+    });
+
+    gulp.task('watch-assets', function() {
+        for (var task in srcPaths) {
+            if (_.isFunction(srcPaths[task])) {
+                srcPaths[task].apply(this);
+            } else {
+                gulp.watch(srcPaths[task], [task]);
+            }
+        }
+    });
 }
 
 module.exports = {
